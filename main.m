7363c34792e5ef94ac7a049e38f3cde00734c6eb
Victoria Wyatt <vicrwyatt@gmail.com>
2023-10-27 17:45:33 -0600

    Updated plots with weighted

--- main.m
+++ main.m
@@ -9,47 +9,67 @@ static_test = readmatrix('Static Test Stand Calibration Case 1.xlsx');
 %convert mv to lbs 
 
 weight = static_test(:,1);
-F1 = static_test(:,4);
-F2 = static_test(:,5);
+F1 = static_test(:,4) - static_test(:,2);
+F2= static_test(:,5)- static_test(:,1);
 
-plot(F2,weight, 'Color', 'g')
+Weight_T = static_test(:,1) .* ( F1 ./ ( F1 + F2) ) 
+Weight_T2 = static_test(:,1) .* ( F2 ./ ( F1 + F2) ) 
+
+figure(3)
+tiledlayout(2,1)
+nexttile
+scatter(F1,Weight_T )
 hold on
-plot(F1,weight)
-legend('F2','F1')
-hold off 
+legend('F1')
+
+nexttile
+scatter(F2,Weight_T2, 'Color', 'g')
+legend('F2')
 
 polyorder = 1;
-[F1poly,S1] = polyfit(F1, weight, polyorder);
-[F2poly,S2] = polyfit(F2, weight, polyorder);
+[F1poly,S1] = polyfit(F1, Weight_T, polyorder);
+[F2poly,S2] = polyfit(F2, Weight_T2, polyorder);
 
 x = 0:max(F1)+10;
 x2 = 0:max(F2)+20;
 [weight_fit1, delta1] = polyval(F1poly, x, S1);
 [weight_fit2, delta2] = polyval(F2poly,x2, S2);
 
+sigma_y1 = sqrt(1/(length(x)-2) * sum((weight_fit1-Weight_T).^2));
+sigma_y2 = sqrt(1/(length(x2)-2) * sum((weight_fit2-Weight_T2).^2));
+
 
 figure(1)
-scatter(F1, weight)
+scatter(F1, Weight_T)
 hold on
 plot(x,weight_fit1)
+plot(x,weight_fit1+2.*sigma_y1)
+plot(x,weight_fit1-2.*sigma_y1)
+xlabel("Volts (mV)")
+ylabel("Weight (lbs)")
 
 figure(2)
-scatter(F2,weight )
+scatter(F2,Weight_T2)
 hold on
 plot(x2,weight_fit2)
+plot(x2,weight_fit2+2.*sigma_y2)
+plot(x2,weight_fit2-2.*sigma_y2)
+xlabel("Volts (mV)")
+ylabel("Weight (lbs)")
+
+
+
+
 
 hold off
+
+% Use f offset for uncertainty for voltage 
+% 
+% Need to know how to propogate error through the whole problem
+
 
 %below here is test stuff  
 
 %testing commit b44
-sigma_y1 = sqrt(1/(length(x)-2) * sum((weight_fit1-weight).^2));
-sigma_y2 = sqrt(1/(length(x2)-2) * sum((weight_fit2-weight).^2));
-figure(3)
-plot(x,weight_fit1+2.*sigma_y1)
-hold on
-plot(x2,weight_fit2+2.*sigma_y2)
-figure(4)
-hold on
-plot(x,weight_fit1.*sigma_y1)
-plot(x2,weight_fit2.*sigma_y2)
+
+
