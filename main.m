808c95e1b5b1ea89da7d310f9fd99e3a01e7f63e
Graham Dobbie <comradegavriil@gmail.com>
2023-10-30 16:08:26 -0600

    stuff

--- main.m
+++ main.m
@@ -43,7 +43,7 @@ hold off
 %below here is test stuff 
 sigma_y1 = sqrt(1/(length(x)-2) * sum((weight_fit1-weight).^2));
 sigma_y2 = sqrt(1/(length(x2)-2) * sum((weight_fit2-weight).^2));
-figure(3)
+figure(3);
 plot(x,weight_fit1+2.*sigma_y1)
 hold on
 plot(x2,weight_fit2+2.*sigma_y2)
@@ -62,21 +62,33 @@ raw_data = load("testrun1.mat");
 F1_data = load_cell_data(:,1);
 F2_data = load_cell_data(:,2);
 
-fit_model_F1 = @(load_cell_voltage) mvToForce(load_cell_voltage, f1_poly);
-fit_model_F2 = @(load_cell_voltage) mvToForce(load_cell_voltage, f2_poly);
+fit_model_F1 = @(load_cell_voltage) mvToForce(load_cell_voltage, F1poly);
+fit_model_F2 = @(load_cell_voltage) mvToForce(load_cell_voltage, F2poly);
 
 F1_load = fit_model_F1(F1_data);
 F2_load = fit_model_F2(F2_data);
 
 
-figure(3);
+figure(5);
 hold on;
 
-plot(time_data, F1_load, "b-", time_data, F2_load, "g-");
+plot(time_data, F1_load, "b-", time_data, F2_load, "r-");
 xlabel("Time since launch (s)");
 ylabel("Thrust (lb)");
-legend("F1 Thrust", "F2_Thrust");
+legend("F1 Thrust", "F2 Thrust");
+title("Total Ploted Thrust Data over Time")
 
+cropped_time_data = transpose(time_data((time_data > 1.80) & (time_data < 2.5)));
+cropped_F1_data = F1_load((time_data > 1.80) & (time_data < 2.5));
+cropped_F2_data = F2_load((time_data > 1.80) & (time_data < 2.5));
+
+figure(6);
+hold on;
+plot( cropped_time_data, cropped_F1_data, "b-", cropped_time_data, cropped_F2_data, "r-");
+xlabel("Time since launch (s)");
+ylabel("Thrust (lb)");
+legend("F1 Thrust", "F2 Thrust");
+title("Cropped Ploted Thrust Data over Time")
 
 
 
